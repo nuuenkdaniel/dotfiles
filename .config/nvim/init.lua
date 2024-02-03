e8cfccd659c6caa9c13d30b63b40ff2cbfbed4d9
Daniel <lewdaniel2@gmail.com>
2024-02-03 15:30:43 -0500

    added treesitter colorscheme and neotree for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,3 @@
-vim.cmd("set number")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -7,19 +5,13 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable", 
     lazypath,
   })
 end
+
+require("options")
+
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	{ 
-		"catppuccin/nvim", name = "catppuccin", priority = 1000 
-	}
-},
-{
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme("catppuccin")
+require("lazy").setup("plugins")
