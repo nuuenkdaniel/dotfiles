b007f174107e902eb8433bdbe2b5f551b5817052
Daniel <lewdaniel2@gmail.com>
2024-02-03 00:28:00 -0500

    customized nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,25 @@
+vim.cmd("set number")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{ 
+		"catppuccin/nvim", name = "catppuccin", priority = 1000 
+	}
+},
+{
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
