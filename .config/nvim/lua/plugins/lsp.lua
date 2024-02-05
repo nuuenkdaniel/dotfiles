7e1dba1353905d795fd9b3c7ab7f2d7166deddb4
Daniel <lewdaniel2@gmail.com>
2024-02-04 23:50:01 -0500

    added changes from laptop nvim conf

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return {
+    {
+    	"williamboman/mason.nvim",
+    	config = function()
+		require("mason").setup()
+    	end
+    },
+    {
+	"williamboman/mason-lspconfig.nvim",
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "jdtls", "quick_lint_js", "jedi_language_server", "html" }
+		})
+	end
+    },
+    {
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({})
+		lspconfig.jdtls.setup({})
+		lspconfig.quick_lint_js.setup({})
+		lspconfig.jedi_language_server.setup({})
+		lspconfig.html.setup({})
+	end
+    }
+}
