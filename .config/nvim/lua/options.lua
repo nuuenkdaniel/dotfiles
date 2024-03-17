0304b8a47d0c8346312bf1d123307e713ada117a
Daniel <lewdaniel2@gmail.com>
2024-03-17 11:37:02 -0400

    update dot

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,1 +1,5 @@
 vim.cmd("set number")
+vim.cmd("set tabstop=2")
+vim.cmd("set smartindent")
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=2")
---
+++ .local/bin/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import os
+
+
+cwd = os.getcwd()
+
+print(cwd)
