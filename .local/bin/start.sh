f83c57fd4cc209e2155b862982aa5cfc46210d3a
Daniel <lewdaniel2@gmail.com>
2024-07-27 16:28:22 -0400

    start script runs playit on start

--- .local/bin/start.sh
+++ .local/bin/start.sh
@@ -3,17 +3,12 @@
 if [[ -n "$SSH_CLIENT" ]]; then
 	echo "In SSH Session, WM's will not run"
 elif [[ "$(tty)" == "/dev/tty3" ]]; then
-	rm -r /home/Danuu/.local/share/applications
-	cp -r /home/Danuu/.local/share/applications_X /home/Danuu/.local/share/applications
 	exec startx
 elif [[ "$(tty)" == "/dev/tty2" ]]; then
-	rm -r /home/Danuu/.local/share/applications
-	cp -r /home/Danuu/.local/share/applications_wayland /home/Danuu/.local/share/applications
 	exec sway
 elif [[ "$(tty)" == "/dev/tty1" ]]; then
-	rm -r /home/Danuu/.local/share/applications
-	cp -r /home/Danuu/.local/share/applications_wayland /home/Danuu/.local/share/applications
 	exec Hyprland
+  exec ~/.local/bin/playit_start
 else
 	echo "In $(tty), WM's will not start on launch"
 fi
