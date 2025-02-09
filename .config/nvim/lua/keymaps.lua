9d89507b092da89866271e5e6916fb634b7565ec
Daniel <lewdaniel2@gmail.com>
2025-02-09 13:51:10 -0500

    Added tabs plugin to neovim

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,5 +1,8 @@
 -- Telescope keybinds
-vim.keymap.set('n', '<C-p>', ':Neotree toggle right<CR>', {})
+vim.keymap.set({ 'n', 'v' }, "<C-f>", ":Telescope find_files<cr>", {})
+
+-- Neotree keybinds
+vim.keymap.set('n', '<C-p>', ":Neotree toggle right<cr>", {})
 
 -- LSP keybinds
 vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, {})
@@ -8,3 +11,18 @@ vim.keymap.set({ 'n', 'v' }, '<C-a>', vim.lsp.buf.code_action, {})
 -- Ollama keybinds
 vim.keymap.set({ 'n', 'v' }, '<leader>oo', ":<c-u>lua require('ollama').prompt()<cr>", {})
 vim.keymap.set({ 'n', 'v' }, "<leader>og", ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>", {})
+
+-- Tab keybinds
+vim.keymap.set('n', "<A-h>", ":BufferPrevious<cr>", {})
+vim.keymap.set('n', "<A-l>", ":BufferNext<cr>", {})
+vim.keymap.set('n', "<A-1>", ":BufferGoto 1<cr>", {})
+vim.keymap.set('n', "<A-2>", ":BufferGoto 2<cr>", {})
+vim.keymap.set('n', "<A-3>", ":BufferGoto 3<cr>", {})
+vim.keymap.set('n', "<A-4>", ":BufferGoto 4<cr>", {})
+vim.keymap.set('n', "<A-5>", ":BufferGoto 5<cr>", {})
+vim.keymap.set('n', "<A-6>", ":BufferGoto 6<cr>", {})
+vim.keymap.set('n', "<A-7>", ":BufferGoto 7<cr>", {})
+vim.keymap.set('n', "<A-8>", ":BufferGoto 8<cr>", {})
+vim.keymap.set('n', "<A-9>", ":BufferGoto 9<cr>", {})
+vim.keymap.set('n', "<A-0>", ":BufferLast<cr>", {})
+vim.keymap.set('n', "<A-w>", ":BufferClose<cr>", {})
--- .config/waybar/bin/g502-bat.sh
+++ .config/waybar/bin/g502-bat.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sleep 3
+
 percentage=$(upower -i /org/freedesktop/UPower/devices/battery_hidpp_battery_0 | grep "percentage" | awk '{print $2}')
 name=$(upower -i /org/freedesktop/UPower/devices/battery_hidpp_battery_0 | grep "model" | awk '{print $2}')
 
